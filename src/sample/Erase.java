2ade54c301544f8f618258bc930c9bb24562cc79
Alaa Yasser <alaa389yasser@gmail.com>
2019-04-15 14:26:57 +0200

    Erase Init

--- src/sample/Erase.java
+++ src/sample/Erase.java
@@ -1,4 +1,31 @@
 package sample;
 
-public class Erase {
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+public class Erase extends Tool{
+
+    private GraphicsContext graphics;
+
+    @Override
+    public void setCanvas(Canvas canvas) {
+        this.canvas = canvas;
+        graphics= this.canvas.getGraphicsContext2D();
+
+        canvas.setOnMousePressed(e -> {
+            graphics.beginPath();
+            graphics.stroke();
+        });
+        canvas.setOnMouseDragged(e ->{
+            super.initX = e.getX();
+            super.initY = e.getY();
+            draw();
+            graphics.stroke();
+        });
+    }
+
+    @Override
+    public void draw() {
+        graphics.clearRect(super.initX, super.initY, 15, 15);
+    }
 }
