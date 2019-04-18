3a9edbdea159d3bf3c6440ce8bb48fd3c0168509
Alaa Yasser <alaa389yasser@gmail.com>
2019-04-18 19:38:59 +0200

    RectangleShape Init

---
+++ src/sample/RectangleShape.java
@@ -0,0 +1,71 @@
+package sample;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.*;
+
+public class RectangleShape extends Shape {
+
+    private StackPane stackPane;
+    private GraphicsContext graphics;
+    private Rectangle rect;
+    private Rectangle rectChange;
+
+    public RectangleShape () {
+        rect = new Rectangle();
+        rectChange = new Rectangle();
+    }
+
+    @Override
+    public void setLayout(StackPane stackPane) {
+        this.stackPane = stackPane;
+    }
+
+    @Override
+    public void setCanvas(DrawCanvas canvas) {
+        this.canvas = canvas;
+        graphics = this.canvas.getGraphicsContext2D();
+
+        canvas.setOnMousePressed(e -> {
+            rect.setX(e.getX());
+            rect.setY(e.getY());
+            rectChange.setX(rect.getX());
+            rectChange.setY(rect.getY());
+        });
+
+        canvas.setOnMouseDragged(e -> {
+            stackPane.getChildren().remove(0);
+            final DrawCanvas tempCanvas = new DrawCanvas();
+            final GraphicsContext gc = tempCanvas.getGraphicsContext2D();
+            tempCanvas.widthProperty().bind(stackPane.widthProperty());
+            tempCanvas.heightProperty().bind(stackPane.heightProperty());
+
+            rectChange.setWidth(Math.abs((e.getX() - rectChange.getX())));
+            rectChange.setHeight(Math.abs((e.getY() - rectChange.getY())));
+
+            if(rectChange.getX() > e.getX()) {
+                rect.setX(e.getX());
+
+            }
+
+            if(rectChange.getY() > e.getY()) {
+                rect.setY(e.getY());
+            }
+
+            gc.setLineWidth(super.LINE_WIDTH);
+            gc.strokeRect(rect.getX(), rect.getY(), rectChange.getWidth(), rectChange.getHeight());
+            stackPane.getChildren().add(0, tempCanvas);
+        });
+
+        canvas.setOnMouseReleased(e -> {
+            draw();
+            canvas.setIsEdited(true);
+        });
+    }
+
+    @Override
+    public void draw() {
+        graphics.setLineWidth(super.LINE_WIDTH);
+        graphics.strokeRect(rect.getX(), rect.getY(), rectChange.getWidth(), rectChange.getHeight());
+    }
+}
