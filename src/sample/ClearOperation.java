fd89802946566b84de212dc8eb3d9b51e1111948
Alaa Yasser <alaa389yasser@gmail.com>
2019-04-18 10:53:51 +0200

    Improve Clear Operation

--- src/sample/ClearOperation.java
+++ src/sample/ClearOperation.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
 
 public class ClearOperation extends Operation {
 
     private GraphicsContext graphics;
+    private StackPane stackPane;
+
+    public void setLayout (StackPane stackPane){
+        this.stackPane = stackPane;
+    }
 
     @Override
     public void setCanvas(DrawCanvas canvas) {
@@ -14,8 +20,10 @@ public class ClearOperation extends Operation {
 
     @Override
     public void operate() {
-
-        graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         
+        stackPane.getChildren().remove(0);
+        stackPane.getChildren().add(0, new DrawCanvas());
+        graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
     }
 }
