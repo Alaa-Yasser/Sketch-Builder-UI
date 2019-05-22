3a78551309ae377d2b20ba2893b11f96699be1a0
Alaa Yasser <alaa389yasser@gmail.com>
2019-05-22 08:40:33 +0200

    Create OpenImageFrame

---
+++ src/main/controller/OpenImageFrame.java
@@ -0,0 +1,45 @@
+package main.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.*;
+
+import java.io.*;
+
+public class OpenImageFrame extends Stage {
+
+    private FXMLLoader loader;
+    private Parent root = null;
+    private Scene scene;
+    private File imageFile;
+    private double sceneWidth;
+    private double sceneHeight;
+
+    public OpenImageFrame (File imageFile) {
+
+        this.imageFile = imageFile;
+
+        try {
+            loader = new FXMLLoader (getClass().getResource("/resources/fxml/OpenImageFrame.fxml"));
+            root = loader.load();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ((OpenImageFrameController)loader.getController()).setImageFile(imageFile);
+        sceneWidth = ((OpenImageFrameController)loader.getController()).getImageWidth();
+        sceneHeight = ((OpenImageFrameController)loader.getController()).getImageHeight();
+
+        scene = new Scene(root, 700, 700);
+
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.initStyle(StageStyle.UNDECORATED);
+        this.setScene(scene);
+        //this.setMaximized(true);
+        this.show();
+
+
+    }
+
+}
---
+++ src/main/controller/OpenImageFrameController.java
@@ -0,0 +1,137 @@
+package main.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class OpenImageFrameController {
+
+    @FXML
+    Pane topBorder;
+    @FXML
+    Pane bottomBorder;
+    @FXML
+    Pane rightBorder;
+    @FXML
+    Pane leftBorder;
+    @FXML
+    ScrollPane scrollPane;
+    @FXML
+    HBox titleBar;
+    @FXML
+    FontIcon closeIcon;
+    @FXML
+    FontIcon maximizeIcon;
+    @FXML
+    FontIcon minusIcon;
+    @FXML
+    Text title;
+    @FXML
+    StackPane imageStackPane;
+
+    private double xOffset;
+    private double yOffset;
+
+    private ImageView imageView;
+    private BufferedImage openedImageBuffer;
+    private Image openedImage;
+    private File imageFile;
+
+    public void initialize() {
+
+        titleBar.setOnMousePressed(e -> {
+            xOffset = e.getSceneX();
+            yOffset = e.getSceneY();
+        });
+
+        titleBar.setOnMouseDragged(event -> {
+            Stage stage = ((Stage)titleBar.getScene().getWindow());
+
+            if(stage.isMaximized())
+                stage.setMaximized(false);
+
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
+        titleBar.setOnMouseClicked(event -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+                if (event.getClickCount() == 2)
+                    maximizeWindow();
+            }
+        });
+
+        closeIcon.setOnMousePressed(event -> {
+            closeIcon.getScene().getWindow().hide();
+        });
+        maximizeIcon.setOnMousePressed(event -> maximizeWindow());
+
+        minusIcon.setOnMousePressed(event -> ((Stage)(minusIcon.getScene().getWindow())).setIconified(true));
+
+    }
+
+    public void setImageFile (File imageFile) {
+        this.imageFile = imageFile;
+
+        title.setText(imageFile.getName());
+
+        try {
+            imageView = new ImageView();
+            openedImage = new Image(new FileInputStream(imageFile));
+            openedImageBuffer = ImageIO.read(imageFile);
+
+
+            if (openedImage.getWidth() > 600)
+                imageView.setFitWidth(openedImage.getWidth() * (0.3));
+            else
+                imageView.setFitWidth(openedImage.getWidth());
+
+            if (openedImage.getHeight() > 600)
+                imageView.setFitHeight(openedImage.getHeight() * (0.3));
+            else
+                imageView.setFitHeight(openedImage.getHeight());
+
+            imageView.setImage(openedImage);
+            imageView.setPreserveRatio(true);
+            imageView.setSmooth(true);
+            imageView.setCache(true);
+
+            imageStackPane.getChildren().add(imageView);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+
+    public double getImageWidth () {return openedImageBuffer.getWidth();}
+    public double getImageHeight () {return openedImageBuffer.getHeight();}
+
+    private void maximizeWindow () {
+        Stage stage = ((Stage)titleBar.getScene().getWindow());
+        if (stage.isMaximized()){
+            topBorder.setCursor(Cursor.V_RESIZE);
+            bottomBorder.setCursor(Cursor.V_RESIZE);
+            leftBorder.setCursor(Cursor.H_RESIZE);
+            rightBorder.setCursor(Cursor.H_RESIZE);
+            stage.setMaximized(false);
+        }else {
+            topBorder.setCursor(Cursor.DEFAULT);
+            bottomBorder.setCursor(Cursor.DEFAULT);
+            leftBorder.setCursor(Cursor.DEFAULT);
+            rightBorder.setCursor(Cursor.DEFAULT);
+            stage.setMaximized(true);
+        }
+    }
+}
