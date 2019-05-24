6ccadd3deb126d20083ef6acc9f94fd04af48935
Alaa Yasser <alaa389yasser@gmail.com>
2019-05-24 22:42:03 +0200

    Create LoadFrame

---
+++ src/main/controller/LoadFrame.java
@@ -0,0 +1,37 @@
+package main.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.*;
+
+import java.io.IOException;
+
+public class LoadFrame extends Stage {
+
+    private Parent root;
+    private Scene scene;
+
+    public LoadFrame () {
+
+        try {
+            root = FXMLLoader.load(getClass().getResource("/resources/fxml/LoadFrame.fxml"));
+            scene = new Scene(root, 600, 330);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.setScene(scene);
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.initStyle(StageStyle.UNDECORATED);
+        this.setResizable(false);
+    }
+
+    public void showStage () {
+        this.show();
+    }
+
+    public void hideStage () {
+        this.hide();
+    }
+}
--- src/main/controller/Main.java
+++ src/main/controller/Main.java
@@ -5,16 +5,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static Client client;
+    public static LoadFrame loadFrame;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        loadFrame = new LoadFrame();
         GalleryFrame galleryFrame = new GalleryFrame();
 
     }
 
     public static void main(String[] args) {
         client = new Client("127.0.0.1", 1996);
+
 //        client.connect();
         launch(args);
 //        client.disconnect();
