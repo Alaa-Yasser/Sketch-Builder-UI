38512a51e93ce955620357dde3f503bebcfd1a80
Alaa Yasser <alaa389yasser@gmail.com>
2019-05-25 22:46:31 +0200

    Delete unused icons

--- src/main/Frame/AboutFrame.java
+++ src/main/Frame/AboutFrame.java
@@ -1,4 +1,41 @@
 package main.Frame;
 
-public class AboutFrame {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import main.controller.Main;
+import main.controller.OpenImageFrameController;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AboutFrame extends Stage {
+
+    FXMLLoader loader;
+    Parent root;
+    Scene scene;
+
+    public AboutFrame () {
+
+        try {
+            loader = new FXMLLoader(getClass().getResource("/resources/fxml/AboutFrame.fxml"));
+            root = loader.load();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        scene = new Scene(root);
+
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.initStyle(StageStyle.UNDECORATED);
+        this.setScene(scene);
+        this.show();
+        Main.openStage(this, 600, 616);
+
+    }
 }
